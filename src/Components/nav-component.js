635a494e973d6ff2da1f0098d24e998056c44621
oscar anillo <oscarani14@gmail.com>
2023-01-25 15:30:16 -0500

    updated cart styles

--- src/Components/nav-component.js
+++ src/Components/nav-component.js
@@ -71,6 +71,13 @@ export const NavBarComponent = ({
         )}
       </div>
       <div className="desktop">
+        {addCart.length !== 0 && (
+          <div className="cart-number">
+            {addCart.map((x) => (
+              <p key={x.id}>{x.value}</p>
+            ))}
+          </div>
+        )}
         <img src="images/logo.svg" alt="" />
       </div>
       <div className="desktop list-items">
@@ -83,8 +90,50 @@ export const NavBarComponent = ({
         </ul>
       </div>
       <div className="desktop">
-        <img src="images/icon-cart.svg" alt="" />
+        <img
+          src="images/icon-cart.svg"
+          alt=""
+          className="desktop-cart"
+          onClick={showCartMenuHandler}
+        />
         <img src="images/image-avatar.png" alt="" className="desktop-avatar" />
+        {showCartMenu && (
+          <div className="cart-menu-mobile">
+            <div>
+              <p className="cart-p">Cart</p>
+              <hr />
+              {addCart.length !== 0 ? (
+                <div className="cart-menu-row">
+                  <div>
+                    <img
+                      src="images/image-product-1-thumbnail.jpg"
+                      alt=""
+                      className="thumbnail"
+                    />
+                  </div>
+                  <div>
+                    <p>Fall Limited Edition Sneakers</p>
+                    <p>
+                      $125.00 x{" "}
+                      {addCart.map((x) => (
+                        <span>{x.value}</span>
+                      ))}
+                      = <strong>{productAmount}.00</strong>
+                    </p>
+                  </div>
+                  <div>
+                    <img src="images/icon-delete.svg" alt="" />
+                  </div>
+                </div>
+              ) : (
+                <p>Your cart is empty</p>
+              )}
+              {addCart.length !== 0 && (
+                <button className="checkout-btn">Checkout</button>
+              )}
+            </div>
+          </div>
+        )}
       </div>
       {showMobileMenu ? (
         <div className="main-open-menu-mobile">
